c575e2d2d6db67b612b40840707c0259ebc5312d
Collin Wilkins <cwilki32@Collins-MBP.attlocal.net>
2022-02-16 10:58:47 -0500

    Initiating RockPaperScissors game

---
+++ src/com/company/Opponent.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class Opponent {
+    // create an opponent array or if loop
+    // random int to generate opponent move
+}
---
+++ src/com/company/RockPaperScissors.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class RockPaperScissors {
+
+    public static void main(String[] args) {
+	//
+    // Use a while loop to run through choices
+    // Set up Scanner for user turn
+        //compare user move to opponent move
+        //Create class called check winner (scoreboard option?)
+    }
+}
