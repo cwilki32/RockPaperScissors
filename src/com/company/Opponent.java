b2c27d09ec392030b74d043817980978d5571850
Collin Wilkins <cwilki32@Collins-MBP.attlocal.net>
2022-02-16 12:36:54 -0500

    Determine result v1.0

--- src/com/company/Opponent.java
+++ src/com/company/Opponent.java
@@ -1,6 +1,68 @@
 package com.company;
 
+import java.util.Random;
+
 public class Opponent {
     // create an opponent array or if loop
     // random int to generate opponent move
-}
+
+    public Random rand = new Random();
+    private String[] moves = {"Rock", "Paper", "Scissors"};
+
+    //set opponent move
+    public String getOpponentMove() {
+        //String response;
+        int opponentMove = rand.nextInt(3);
+        //response = intToString(opponentMove);
+        return moves[opponentMove];
+
+    }
+    //create method to call int to string
+    /*public String intToString(int i) {
+        if (i == 0)
+            return "Rock";
+        else if (i == 1)
+            return "Paper";
+        else if (i == 2)
+            return "Scissors";
+        else
+            return "Please enter another choice."; //attempt 1
+        }*/
+
+    //write method called compare that compares user choice to opponent move
+    public int didUserWin(String user, String computer) {
+        if (user.equals(computer))
+            return 0;
+        else if (user.equals("Rock")) {
+            if (computer.equals("Scissors"))
+                return 1;
+            else if (computer.equals("Paper"))
+                return -1;
+        }
+        else if (user.equals("Scissors")) {
+                if (computer.equals("Rock"))
+                    return -1;
+                else if (computer.equals("Paper"))
+                    return 1;
+        }
+        else if (user.equals("Paper")) {
+                if (computer.equals("Rock"))
+                    return 1;
+                else if (computer.equals("Scissors"))
+                    return -1;
+        }
+         {
+                System.out.println("Error!");
+                return 2;
+            }
+        }
+        //declare scoreboard method
+        public void scoreBoard(int x) {
+            if (x == 0)
+            System.out.println("You tied!");
+            else if (x == 1)
+            System.out.println("You won!");
+            else if (x == -1)
+            System.out.println("You lost!");
+        }
+    }
--- src/com/company/RockPaperScissors.java
+++ src/com/company/RockPaperScissors.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class RockPaperScissors {
 
     public static void main(String[] args) {
@@ -8,5 +10,21 @@ public class RockPaperScissors {
     // Set up Scanner for user turn
         //compare user move to opponent move
         //Create class called check winner (scoreboard option?)
+        //generate computer
+        //Step 2 is to error proof our inputs
+        Opponent computer = new Opponent();
+        //call opponent move with variable named ComputerPlay
+        String computerPlay = computer.getOpponentMove();
+        int isWinner;
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Pick a move: Rock, Paper, or Scissors");
+        String userPlay = userInput.next();
+
+        //print computer move
+        System.out.println("Computer chose " + computerPlay);
+        //declare results
+        isWinner = computer.didUserWin(userPlay, computerPlay);
+        computer.scoreBoard(isWinner);
     }
 }
